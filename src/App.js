39195e9270b18243d1fc78195931a7944afb799f
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-12 22:27:23 -0500

    styled a single component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 
 //special thing about this function is that ir returns jsx code...is a function and custom html element. This is a component and a component must return something that can be rendered in the browser
 //"state" is built into react and lets us change the state of an application
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Lets get Started</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }

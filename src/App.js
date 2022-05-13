6a586ef988ea630c25829240ff036827cf94ffc3
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-12 21:35:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+//special thing about this function is that ir returns jsx code...is a function and custom html element. This is a component and a component must return something that can be rendered in the browser
+//"state" is built into react and lets us change the state of an application
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
